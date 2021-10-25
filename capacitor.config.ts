3d696e84d5c9539a16279a1de88cbe9989082c54
Subin Viswanathan <subinv1992@gmail.com>
2021-10-25 11:11:39 +0530

    add the config and auth changes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.subin.booking-places',
+  appName: 'subin-booking-places',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
